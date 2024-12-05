97e7ab69925fd7f0dbc8d760de483009ca2dce41
joshtorie <yitzmeyer@gmail.com>
2024-12-05 19:07:56 +0200

    export key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://udcdedrjlnloabmhilmb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkY2RlZHJqbG5sb2FibWhpbG1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzMjIzNjAsImV4cCI6MjA0ODg5ODM2MH0.15g1bh7OAT9GPqje6Ld96zwzJ9GiHUHFl3bnctRflEo';
+export const supabaseUrl = 'https://udcdedrjlnloabmhilmb.supabase.co';
+const supabaseKey = 'your-supabase-key';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
